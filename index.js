bd960ccb3f9ea3f7565a902cceca50e76ed00506
Abdul Akhir <80597825+abdulakhir98@users.noreply.github.com>
2023-06-01 14:57:47 +0500

    Organized code

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express')
-const app = express()
-require('./db/conn');
-require('./LoadEnvironment.js')
-const PORT = process.env.PORT
+const express = require("express");
+const app = express();
+require("./db/conn");
+require("./config/LoadEnvironment.js");
+const PORT = process.env.PORT;
 
 //middleware
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const productRoute = require('./routes/Products');
+const productRoute = require("./routes/Products");
 
 app.use("/", productRoute);
 
 // Home Route!
 
-app.get("/",(req, res) => {
-    res.send("Hello World")
-})
-
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(PORT, () => {
-    console.log(`NodeJS API is running on port: ${PORT}`)
-})
+    console.log(`NodeJS API is running on port: ${PORT}`);
+});
